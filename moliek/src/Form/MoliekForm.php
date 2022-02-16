cc739b34c0cf41083ab08558b56eb47a5e92ace5
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2022-02-16 17:47:01 +0200

    Improved code, added validation to empty cells

--- moliek/src/Form/MoliekForm.php
+++ moliek/src/Form/MoliekForm.php
@@ -37,12 +37,15 @@ class MoliekForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state): array {
     $form['#prefix'] = '<div id="moliek-form">';
     $form['#suffix'] = '</div>';
+    $form['#attached']['library'][] = 'moliek/style';
+
     $this->createTable($form, $form_state);
 
     $form['add_table'] = [
       '#type' => 'submit',
       '#value' => $this->t("Add Table"),
       '#submit' => ['::addTable'],
+      '#name' => 'add_table',
       '#ajax' => [
         'event' => 'click',
         'progress' => 'none',
@@ -53,9 +56,15 @@ class MoliekForm extends FormBase {
 
     $form['submit'] = [
       '#type' => 'submit',
+      '#name' => 'submit',
       '#value' => $this->t("Submit"),
+      '#ajax' => [
+        'event' => 'click',
+        'progress' => 'none',
+        'callback' => '::refreshAjax',
+        'wrapper' => 'moliek-form',
+      ],
     ];
-    $form['#attached']['library'][] = 'moliek/style';
 
     return $form;
   }
@@ -69,9 +78,9 @@ class MoliekForm extends FormBase {
    *   The current state of the form.
    *
    * @return array
-   *   The form structure.
+   *   The form structures.
    */
-  public function createTable(array &$form, FormStateInterface $form_state) {
+  public function createTable(array &$form, FormStateInterface $form_state): array {
     $header_title = [
       'year' => $this->t('Year'),
       'jan' => $this->t('Jan'),
@@ -93,58 +102,57 @@ class MoliekForm extends FormBase {
       'ytd' => $this->t('YTD'),
     ];
     for ($t = 0; $t < $this->countTable; $t++) {
-      $form["table_$t"] = [
-        'actions' => [
-          '#type' => 'actions',
-          'button' => [
-            '#type' => 'submit',
-            '#name' => "add_row_$t",
-            '#value' => $this->t("Add Year"),
-            '#submit' => ['::addRow'],
-            '#data_id' => $t,
-            '#ajax' => [
-              'event' => 'click',
-              'progress' => 'none',
-              'callback' => '::refreshAjax',
-              'wrapper' => 'moliek-form',
-            ],
-          ],
-        ],
-        'table' => [
-          '#type' => 'table',
-          '#header' => $header_title,
-          '#empty' => t('No content available.'),
+      $form["button_$t"] = [
+        '#type' => 'submit',
+        '#name' => $t,
+        '#value' => $this->t("Add Year"),
+        '#submit' => ['::addRow'],
+        '#ajax' => [
+          'event' => 'click',
+          'progress' => 'none',
+          'callback' => '::refreshAjax',
+          'wrapper' => 'moliek-form',
         ],
       ];
-      for ($r = 0; $r < $this->countRow[$t]; $r++) {
+      $form["table_$t"] = [
+        '#type' => 'table',
+        '#header' => $header_title,
+        '#empty' => t('No content available.'),
+      ];
+      for ($r = $this->countRow[$t]; $r > 0; $r--) {
         foreach ($header_title as $c) {
-          $form["table_$t"]['table']["rows_$r"]["$c"] = [
+          $form["table_$t"]["rows_$r"]["$c"] = [
             '#type' => 'number',
           ];
           if (in_array("$c", ['Q1', 'Q2', 'Q3', 'Q4', 'YTD'])) {
-            $form["table_$t"]['table']["rows_$r"]["$c"] = [
+            $form["table_$t"]["rows_$r"]["$c"] = [
               '#type' => 'number',
               '#disabled' => TRUE,
             ];
-          }
-          if ("$c" == 'Year') {
-            $form["table_$t"]['table']["rows_$r"]["$c"] = [
-              '#type' => 'number',
-              '#disabled' => TRUE,
-              '#default_value' => date('Y') - $this->countRow[$t] + $r + 1,
-            ];
-
           }
         }
+        $form["table_$t"]["rows_$r"]['Year'] = [
+          '#type' => 'number',
+          '#disabled' => TRUE,
+          '#default_value' => date('Y') - $r + 1,
+        ];
       }
     }
     return $form;
   }
 
   /**
+   * Add a table.
    *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   The form structures.
    */
-  public function addTable(array $form, FormStateInterface $form_state) {
+  public function addTable(array $form, FormStateInterface $form_state): array {
     $this->countTable++;
     $this->countRow[] = 1;
     $form_state->setRebuild();
@@ -152,10 +160,18 @@ class MoliekForm extends FormBase {
   }
 
   /**
+   * Add a row to the table.
    *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   The form structures.
    */
-  public function addRow(array $form, FormStateInterface $form_state) {
-    $t = $form_state->getTriggeringElement()['#data_id'];
+  public function addRow(array $form, FormStateInterface $form_state): array {
+    $t = $form_state->getTriggeringElement()['#name'];
     $this->countRow[$t]++;
     $form_state->setRebuild();
     return $form;
@@ -163,15 +179,68 @@ class MoliekForm extends FormBase {
 
   /**
    * Refresh Ajax.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   The form structures.
    */
-  public function refreshAjax(array $form, FormStateInterface $form_state) {
+  public function refreshAjax(array $form, FormStateInterface $form_state): array {
     return $form;
   }
 
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getTriggeringElement()['#name'] !== 'submit') {
+      return;
+    }
+    $tables = $form_state->getValues();
+    for ($t = 0; $t < $this->countTable; $t++) {
+      $hasValue = 0;
+      $hasEmpty = 0;
+      foreach ($tables["table_$t"] as $r) {
+        foreach ($r as $key => $i) {
+          if (in_array("$key", ['Year', 'Q1', 'Q2', 'Q3', 'Q4', 'YTD'])) {
+            continue;
+          }
+          if (!$hasValue && !$hasEmpty && $i !== "") {
+            $hasValue = 1;
+          }
+          if ($hasValue && !$hasEmpty && $i == "") {
+            $hasEmpty = 1;
+          }
+          if ($hasValue && $hasEmpty && $i !== "") {
+            $form_state->setErrorByName("Empty", 'There should be no gaps in the table.');
+            $this->messenger()->addError('Invalid');
+            break 2;
+          }
+        }
+      }
+      if (!$hasValue && !$hasEmpty) {
+        $this->messenger()->addError('Invalid');
+        $form_state->setErrorByName("Empty table", 'The table should not be empty.');
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getErrors()) {
+      foreach ($form_state->getErrors() as $err) {
+        $this->messenger()->addError($err);
+      }
+      $form_state->clearErrors();
+    }
+    else {
+      $this->messenger()->addStatus("Valid");
+    }
   }
 
 }
